341717081c163e54ad8b8f52abffb97e1cfd7a89
Vikram Mehta <vikrammehta83@gmail.com>
2024-01-26 23:30:54 -0500

    Added some classes and styles to fix the spacing of no results text

--- src/components/NoResults.jsx
+++ src/components/NoResults.jsx
@@ -4,9 +4,9 @@ const NoResults = (props) => {
     const { handleShowModal, className } = props
 
     return (
-        <div className={`text-center ${className} flex flex-col items-center justify-center`}>
-            <h3 className='text-lg text-cyan-800 font-bold tracking-wider uppercase mb-2'>Congrats!! You completed all your tasks!</h3>
-            <p className='px-7 mb-3 font-light leading-relaxed'>Well done! You're officially a task ninja. Now, sit back, relax, and bask in the glory of your empty to-do list. 🏖️"</p>
+        <div className={`text-center ${className} px-10 pb-20`}>
+            <h3 className='text-lg text-cyan-800 font-bold tracking-wider uppercase mb-2 mt-20'>Congratulations!! You completed all your tasks!</h3>
+            <p className='mb-3 font-light leading-relaxed'>Well done! You're officially a task ninja. Now, sit back, relax, and bask in the glory of your empty to-do list. 🏖️"</p>
             <p className='mb-3 font-light leading-relaxed'>or, Create a new one</p>
             <button onClick={handleShowModal} className="py-3 px-6 bg-cyan-500 hover:bg-cyan-600 rounded-md text-white">Add Task</button>
         </div>
